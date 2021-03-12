9d965014df514fbcee0c2bc346a80878bfab5a00
thisloving <thisloving@gmail.com>
2021-03-12 12:11:32 +0800

    String to Integer (atoi)

---
+++ 9_palindrome_number/palindrome_number.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	oldX := x
+	y := int(0)
+	for x != 0 {
+		y = y*10 + x%10
+		x = x / 10
+	}
+
+	if oldX == y {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	x := 121
+	is := isPalindrome(x)
+	fmt.Println(is)
+}
